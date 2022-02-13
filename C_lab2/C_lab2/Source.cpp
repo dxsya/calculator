5599ed10453528bdf75388764f271ff0c2997192
dxsya <66560538+dxsya@users.noreply.github.com>
2022-02-13 15:50:35 +0300

    Update Source.cpp

--- C_lab2/C_lab2/Source.cpp
+++ C_lab2/C_lab2/Source.cpp
@@ -6,6 +6,14 @@
 
 typedef long double real;
 
+/*
+	Error _CALC
+	962957572.248576: translate_to_notation error [wrong sequence, example {3, -, ...}]
+	53587291.5352: division on the zero
+	-1532523.6367: _CALC error [wrong result, example {3, 4} or {}]
+	-53518506.2452: _CALC error [wrong result, example {*}]
+*/
+
 typedef struct String
 {
 	char* data;
@@ -142,6 +150,43 @@ void push_back(string* str, char n)
 
 	str->data = new_str.data;
 	str->length = new_str.length;
+}
+
+real _CALC(node_arr data)
+{
+	for (int i = 0; i < data.length; ++i)
+	{
+		if (data.arr[i].flag)
+		{
+			real res;
+			if (i < 2)
+				return 962957572.248576;
+
+			if (data.arr[i].operation == '+')
+				res = data.arr[i - 2].number + data.arr[i - 1].number;
+			else if (data.arr[i].operation == '-')
+				res = data.arr[i - 2].number - data.arr[i - 1].number;
+			else if (data.arr[i].operation == '*')
+				res = data.arr[i - 2].number * data.arr[i - 1].number;
+			else if (data.arr[i].operation == '/')
+			{
+				if (data.arr[i - 1].number == 0)
+					return 53587291.5352;
+				res = data.arr[i - 2].number / data.arr[i - 1].number;
+			}
+			else
+				res = pow(data.arr[i - 2].number, data.arr[i - 1].number);
+			data.arr[i - 2].number = res;
+			na_erase(&data, i - 1, 2);
+			i -= 2;
+		}
+	}
+	if (data.length != 1)
+		return -1532523.6367;
+	else if (data.arr[0].flag)
+		return -53518506.2452;
+	else
+		return data.arr[0].number;
 }
 
 int main()
