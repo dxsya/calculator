c66e34607f1868f2751566651f98888fe7f02ce6
progdt <darxtsi@gmail.com>
2022-02-13 16:26:46 +0300

    fix

--- C_lab2/C_lab2/Source.cpp
+++ C_lab2/C_lab2/Source.cpp
@@ -49,6 +49,7 @@ void push_back(string* str, char n)
 
 	str->data = new_str.data;
 	str->length = new_str.length;
+	free(new_str.data);
 }
 
 node_arr translate_to_notation(string* expression)
@@ -233,12 +234,11 @@ void insert(string* str, int index, string under)
 	str->length = new_str.length;
 }
 
-real _CALC(char* task)
+real _CALC(string task)
 {
-	string str;
-	init(&str, task);
-	remove_space(&str);
-	node_arr data = translate_to_notation(&str);
+	
+	remove_space(&task);
+	node_arr data = translate_to_notation(&task);
 	
 	for (int i = 0; i < data.length; ++i)
 	{
@@ -275,6 +275,41 @@ real _CALC(char* task)
 		return data.arr[0].number;
 }
 
-int main()
+char* get_string(int* len) {
+	*len = 0; // изначально строка пуста
+	int capacity = 1; // ёмкость контейнера динамической строки (1, так как точно будет '\0')
+	char* s = (char*)malloc(sizeof(char)); // динамическая пустая строка
+
+	char c = getchar(); // символ для чтения данных
+
+	// читаем символы, пока не получим символ переноса строки (\n)
+	while (c != '\n') {
+		s[(*len)++] = c; // заносим в строку новый символ
+
+		// если реальный размер больше размера контейнера, то увеличим его размер
+		if (*len >= capacity) {
+			capacity *= 2; // увеличиваем ёмкость строки в два раза
+			s = (char*)realloc(s, capacity * sizeof(char)); // создаём новую строку с увеличенной ёмкостью  
+		}
+
+		c = getchar(); // считываем следующий символ          
+	}
+
+	s[*len] = '\0'; // завершаем строку символом конца строки
+
+	return s; // возвращаем указатель на считанную строку
+}
+
+int main() 
 {
+	int len; // длина строки
+	char* s = get_string(&len); // считываем динамическую строку
+
+	string exp;
+	exp.length = len;
+	exp.data = s;
+
+	printf("%lf\n", _CALC(exp));
+
+	return 0;
 }
